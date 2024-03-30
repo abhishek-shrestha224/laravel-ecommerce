ea496b564b48442c20844bfa4c21077b25c2741e
abhishek-shrestha224 <meabhi224@gmail.com>
2024-03-30 14:11:57 +0545

    added form validation and error logging

--- app/Http/Controllers/admin/AdminLoginController.php
+++ app/Http/Controllers/admin/AdminLoginController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AdminLoginController extends Controller
 {
     public function index()
     {
         return view('admin.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $validator = Validator::make($request->all(), ['email' => 'email|required', 'password' => 'required']);
+
+        if ($validator->fails()) {
+            return redirect()->route('admin.login')->withErrors($validator)->withInput($request->only('email'));
+        }
     }
 }
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -15,46 +15,61 @@
                 <h4 class="text-4xl font-bold" href="#">Administrative Panel</h4>
             </div>
             <div class="card-body mt-12">
-                <form class="mx-auto max-w-md" action="dashboard.html" method="POST">
-                    <h5 class="login-box-msg w-full text-center text-2xl font-bold mb-4">Please Login to Continue</h5>
+                <form class="mx-auto max-w-md" action="{{ route('admin.authenticate') }}" method="POST">
+                    @csrf
+                    <h5 class="login-box-msg mb-4 w-full text-center text-2xl font-bold">Please Login to Continue</h5>
                     <div class="input-group group relative z-0 mb-5 w-full">
                         <input
                             class="form-control peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
-                            type="email" placeholder="">
+                            id="email" type="email" name="email" placeholder="">
                         <label
                             class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4 dark:text-gray-400 peer-focus:dark:text-blue-500"
-                            for="floating_email">Email address</label>
+                            for="email">Email address</label>
+                        <div class="w-full">
+                            @error('email')
+                                <p class="text-accent text-sm">{{ $message }}</p>
+                            @enderror
+                        </div>
                     </div>
+
                     <div class="input-group group relative z-0 mb-5 w-full">
                         <input
                             class="form-control peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
-                            type="password" placeholder="">
+                            type="password" name="password" email="password" value="{{old("email")}}" placeholder="">
                         <label
                             class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 rtl:peer-focus:translate-x-1/4 dark:text-gray-400 peer-focus:dark:text-blue-500"
-                            for="floating_password">Password</label>
-
+                            for="password">Password</label>
+                        <div class="w-full">
+                            @error('password')
+                                <p class="text-accent text-sm">{{ $message }}</p>
+                            @enderror
+                        </div>
                     </div>
                     <div class="row">
                         <!-- <div class="col-8">
-                             <div class="icheck-primary">
-                                 <input id="remember" type="checkbox">
-                                 <label for="remember">
-                              Remember Me
-                                 </label>
-                             </div>
-                               </div> -->
+                                                 <div class="icheck-primary">
+                                                     <input id="remember" type="checkbox">
+                                                     <label for="remember">
+                                                  Remember Me
+                                                     </label>
+                                                 </div>
+                                                   </div> -->
                         <!-- /.col -->
-                        <div class="flex gap-4 items-center">
-                            <button class="btn btn-primary btn-block text-light hover:text-white border border-accent hover:bg-accent focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md px-5 py-2.5 text-center me-2 mb-2 dark:border-accent dark:text-accent dark:hover:text-white dark:hover:bg-accent dark:focus:ring-accent" type="submit">Login</button>
+                        <div class="flex items-center gap-4">
+                            <button
+                                class="btn btn-primary btn-block text-light border-accent hover:bg-accent text-md dark:border-accent dark:text-accent dark:hover:bg-accent dark:focus:ring-accent mb-2 me-2 rounded-lg border px-5 py-2.5 text-center font-medium hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:hover:text-white"
+                                type="submit">Login</button>
                             <p class="h-full">
-                                <a href="forgot-password.html" class="font-medium text-md dark:text-accent hover:underline">I forgot my password</a>
+                                <a class="text-md dark:text-accent font-medium hover:underline"
+                                    href="forgot-password.html">I forgot my password</a>
                             </p>
                         </div>
 
                         <!-- /.col -->
                     </div>
+
                 </form>
-               
+
             </div>
             <!-- /.card-body -->
         </div>
