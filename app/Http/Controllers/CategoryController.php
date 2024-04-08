4a858502bfce9505ebc2c003aba3dd665624f60b
abhishek-shrestha224 <meabhi224@gmail.com>
2024-04-08 20:44:56 +0545

    added index action for category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,7 +13,9 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $categories = Category::latest()->paginate(10);
+
+        return view('admin.category.index', compact('categories'));
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::group(['prefix' => 'admin'], function () {
 
         // Categories Route
         Route::group(['prefix' => 'categories'], function () {
+            Route::get('/', [CategoryController::class, 'index'])->name('categories.index');
             Route::get('/create', [CategoryController::class, 'create'])->name('categories.create');
             Route::post('/', [CategoryController::class, 'store'])->name('categories.store');
         });
