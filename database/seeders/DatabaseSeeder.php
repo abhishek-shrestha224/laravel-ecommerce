b92de0985c4d8567ec11e7b82a4d77a3c5d7923d
Abhishek Shrestha <meabhi224@gmail.com>
2024-04-13 14:53:07 +0545

    created migration table for product and product image -blocked

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,11 +16,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'AdminUser',
+            'email' => 'admin@email.com',
+            'password' => 'secret',
+            'role' => 2,
+        ]);
         Category::factory(50)->create();
     }
-    
 }
