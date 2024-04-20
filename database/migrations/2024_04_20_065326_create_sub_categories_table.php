fd7b5facb2397f53e1b2d2adf26648a5f187f45d
Abhishek Shrestha <meabhi224@gmail.com>
2024-04-20 12:41:21 +0545

    added migration for subcategories

--- database/migrations/2024_04_20_065326_create_sub_categories_table.php
+++ database/migrations/2024_04_20_065326_create_sub_categories_table.php
@@ -4,15 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('products_images', function (Blueprint $table) {
+        Schema::create('sub_categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->integer('status')->default(1);
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products_images');
+        Schema::dropIfExists('sub_categories');
     }
 };
